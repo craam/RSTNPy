e3c66e6bdd28786f5f1144d2006c29f0c483e6c7
Edison Neto <ednetoali@gmail.com>
2018-04-12 14:49:44 -0300

    Corrected setup

--- setup.py
+++ setup.py
@@ -4,15 +4,12 @@ from setuptools import setup
 setup(
     name="noaa-ftp",
     version="0.1",
-    description="Download solar data from noaa's FTP",
-    keywords="noaa ftp solar-data solar sagamore hill san vito",
     url="https://github.com/3ldr0n/noaa-ftp",
     author="Edison Neto and Douglas Silva",
     author_email="ednetoali@gmail.com",
     license="GPLv2",
     packages=['noaa-ftp'],
-    install_requires=['wget'],
-    include_data_package=True,
-    zip_safe=False
+    include_package_data=True,
+    install_requires=['wget']
 )
 

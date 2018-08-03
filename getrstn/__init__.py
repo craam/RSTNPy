9f0d21e3e64fa0f3168e31686091b74b105db41d
Edison Neto <ednetoali@gmail.com>
2018-08-03 18:27:20 -0300

    Changes a bit to be uploaded at pypi

--- getrstn/__init__.py
+++ getrstn/__init__.py
@@ -1,3 +1,4 @@
 from .rstn import GetRSTN
 
+name = "getrstn"
 __version__ = "0.2.2"
--- setup.py
+++ setup.py
@@ -1,13 +1,24 @@
 from setuptools import setup
 
+with open("README.md", "r") as readme:
+    long_description = readme.read()
+
 setup(
     name="getrstn",
     version="0.2.2",
     author="Edison Neto and Douglas Silva",
     author_email="ednetoali@gmail.com",
+    description="Downloads rstn-1-second data",
+    long_description=long_description,
+    url="https://github.com/3ldr0n/getrstn",
     packages=['getrstn'],
     include_package_data=True,
     install_requires=[
         'wget'
     ],
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GPLv2",
+        "Operating System :: OS Independent",
+    ),
 )

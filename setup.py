0e05d9329f2fe1a08ac936200a28397c2500b1c3
Edison Neto <ednetoali@gmail.com>
2018-08-04 16:45:14 -0300

    Fixes the setup to be sent to pypi

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ setup(
     ],
     classifiers=(
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: GPLv2",
+        "License :: OSI Approved :: GNU General Public License v2 (GPLv2)",
         "Operating System :: OS Independent",
     ),
 )
